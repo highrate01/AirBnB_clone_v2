08220899c3f5853533315d703b999eb52024b6e5
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-02 15:33:58 +0000

    updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
+import models
+
+STORAGE_TYPE = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='states', cascade='all, delete-orphan')
+
+    if STORAGE_TYPE != 'db':
+        @property
+        def cities(self):
+            """
+            return all cities
+            """
+            city_list = []
+
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
