74afdf9a1f18ff95886de35797f737fd225e8682
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-03 09:22:44 +0000

    updated

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+
 from sqlalchemy import Column, ForeignKey, String
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,6 +69,7 @@ class DBStorage:
         add the object to the current database session
         """
         self.__session.add(obj)
+            
 
     def save(self):
         """
